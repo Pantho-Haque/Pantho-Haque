6120f672a9d4818e469d643f6420e29296549204
Pantho <panthohaque927908@gmail.com>
2023-04-27 23:50:25 +0600

    favicon link modified in document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,10 @@ export default function Document() {
       <Head>
         <title>Pantho Haque</title>
         
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="shortcut apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel='shortcut icon' type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel='shortcut icon' type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest"/>
 
         <script src="https://cdn.jsdelivr.net/npm/kutty@latest/dist/kutty.min.js"></script>

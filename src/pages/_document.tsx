5fdb9625c6492d2adbbb31867a9394827599b130
Pantho <panthohaque927908@gmail.com>
2023-04-28 00:01:13 +0600

    favicon link modified with repo name in document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,10 @@ export default function Document() {
       <Head>
         <title>Pantho Haque</title>
         
-        <link rel="shortcut apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel='shortcut icon' type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel='shortcut icon' type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut apple-touch-icon" sizes="180x180" href="/Pantho-Haque/apple-touch-icon.png"/>
+        <link rel='shortcut icon' type="image/png" sizes="32x32" href="/Pantho-Haque/favicon-32x32.png"/>
+        <link rel='shortcut icon' type="image/png" sizes="16x16" href="/Pantho-Haque/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="/Pantho-Haque/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest"/>
 
         <script src="https://cdn.jsdelivr.net/npm/kutty@latest/dist/kutty.min.js"></script>

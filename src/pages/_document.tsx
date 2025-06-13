69fa57cfa4f59c215fddbfe266f4de8b666b6e48
Pantho Haque <pantho.haque.dev@gmail.com>
2025-06-13 13:45:35 +0600

    fix:title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Head, Html, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 
 export default function Document() {
+  useEffect(() => {
+    document.title = "Pantho Haque | Portfolio";
+  }, []);
   return (
     <Html lang="en">
       <Head>
-        <title>Pantho Haque</title>
-        
+        <title>{document.title}</title>
         <link rel="shortcut apple-touch-icon" sizes="180x180" href="/Pantho-Haque/apple-touch-icon.png"/>
         <link rel='shortcut icon' type="image/png" sizes="32x32" href="/Pantho-Haque/favicon-32x32.png"/>
         <link rel='shortcut icon' type="image/png" sizes="16x16" href="/Pantho-Haque/favicon-16x16.png"/>
